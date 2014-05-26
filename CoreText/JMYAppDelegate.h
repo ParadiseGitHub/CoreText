01b47d22142519ab33673af3f8d59d0f201916c6
ParadiseGitHub <823115753@qq.com>
2014-05-26 12:32:43 +0800

    Initial Commit

---
+++ CoreText/JMYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMYAppDelegate.h
+//  CoreText
+//
+//  Created by jmy on 14-5-26.
+//  Copyright (c) 2014年 jmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreText/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreText
+//
+//  Created by jmy on 14-5-26.
+//  Copyright (c) 2014年 jmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JMYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JMYAppDelegate class]));
+    }
+}
